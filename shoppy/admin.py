e3b0070a433562c30ea72acc0b779bdc32e6cbfa
Aga Misiek <a.misiek@karibuzoo.pl>
2015-06-07 22:53:34 +0100

    Product admin view sorted out

--- shoppy/admin.py
+++ shoppy/admin.py
@@ -7,15 +7,15 @@ from .models import ProductReview
 
 
 class ProductAdmin(admin.ModelAdmin):
-    fields = ["name", "category", "description", "price", "created_at"]
-    list_display = ("name", "category", "price")
+    fields = ["name", "category", "description", "price", "created_by"]
+    list_display = ("name", "category", "price", "created_at", "created_by")
     search_fields = ["name", "category__name"]
     list_filter = ("category__name",)
 
 
 class ProductReviewAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,             {"fields": ["product", "content", "rating"]}),
+        (None, {"fields": ["product", "content", "rating"]}),
         ("Date information", {"fields": ["created_by", "created_at"], "classes": ["collapse"]}),
     ]
 
